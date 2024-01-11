624584ddd30c4b50817829b33129e5a9b27789b4
weihang <pehweihang@live.com>
2024-01-11 12:10:56 +0800

    waybar config

---
+++ .config/waybar/scripts/get_updates.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Check release
+if [ ! -f /etc/arch-release ] ; then
+    exit 0
+fi
+
+# Check for updates
+aur=`paru -Qua | wc -l`
+ofc=`checkupdates | wc -l`
+
+# Calculate total available updates
+upd=$(( ofc + aur ))
+
+# Show tooltip
+if [ $upd -eq 0 ] ; then
+    echo "{\"text\":\"$upd\", \"tooltip\":\" Packages are up to date\"}"
+else
+    echo "{\"text\":\"$upd\", \"tooltip\":\"󱓽 Official: $ofc\n󱓾 AUR: $aur$fpk_disp\"}"
+fi
